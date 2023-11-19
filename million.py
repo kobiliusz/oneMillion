b3e2144256c67ba270b312e50ccb5ebb70bf14a5
kobiliusz <kobiliusz@gmail.com>
2023-11-19 08:50:30 +0100

    function works up to decillions now because why not

--- million.py
+++ million.py
@@ -36,31 +36,49 @@ teens = {
     19: 'nineteen'
 }
 
+big = [
+    'thousand',
+    'million',
+    'billion',
+    'trillion',
+    'quadrillion',
+    'quintillion',
+    'sextillion',
+    'septillion',
+    'octillion',
+    'nonillion',
+    'decillion'
+]
 
-def number_name(no):
+
+def number_name(no: int):
     remainder = ''
-    if no < 1:
-        raise NotImplementedError()
+    if no < 0:
+        return 'minus ' + number_name(-no)
+    if not no:
+        return 'zero'
     elif no < 10:
         return ones[no]
     elif no < 20:
         return teens[no]
     elif no < 100:
         if no % 10:
-            remainder = '-' + number_name(no % 10)
+            remainder = '-' + ones[no % 10]
         return tens[no // 10] + remainder
-    elif no < 1_000:
+    elif no < 1000:
         if no % 100:
             remainder = ' and ' + number_name(no % 100)
         return ones[no // 100] + ' hundred' + remainder
-    elif no < 1_000_000:
-        if no % 1000:
-            remainder = ', ' + number_name(no % 1000)
-        return number_name(no // 1000) + ' thousand' + remainder
     else:
-        raise NotImplementedError()
+        for exponent, name in enumerate(big, start=1):
+            if no < 1000 ** (exponent + 1):
+                if no % (1000 ** exponent):
+                    remainder = ', ' + number_name(no % (1000 ** exponent))
+                return number_name(no // (1000 ** exponent)) + ' ' + name + remainder
+        else:
+            raise NotImplementedError()
 
 
 if __name__ == '__main__':
-    for i in range(1, 1_000_000):
+    for i in range(1, 1_000_001):
         print(number_name(i))
